26eefe3345a9e7972f170747db06e3c9f56dfdbb
Vitor <vitor.nogue316@gmail.com>
2026-04-11 14:56:50 -0300

    Configurando o Curso Types

---
+++ src/modules/curso/curso.types.ts
@@ -0,0 +1,19 @@
+export interface ICurso {
+    name: string;
+    descripton?: string; 
+    active: boolean;
+    createAt?: string;
+    updateAt?: string;
+}
+
+export interface ICreateCursoDTO{
+    name: string;
+    description?: string;
+    active?: boolean;
+}
+
+export interface IUpdateCursoDTO{
+    name?: string;
+    description?: string;
+    active?: boolean;
+}
