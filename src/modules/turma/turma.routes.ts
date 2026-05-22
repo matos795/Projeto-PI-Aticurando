2ebe98cc6d52310d73339c59cdab076a7970a470
Alexandre Matos <matosalexandre795@gmail.com>
2026-05-22 01:23:19 -0300

    Protegendo Rotas de Turmas 2

--- src/modules/turma/turma.routes.ts
+++ src/modules/turma/turma.routes.ts
@@ -1,12 +1,37 @@
 import { Router } from "express";
 import turmaController from "./turma.controller.js";
+import { authorizeRoles, requireAuth } from "../../middlewares/auth.middleware.js";
+import { Papel_usuario } from "../user/user.types.js";
 
 const turmaRoutes = Router();
 
-turmaRoutes.post("/", turmaController.create);
-turmaRoutes.get("/", turmaController.findAll);
-turmaRoutes.get("/:id", turmaController.findById);
-turmaRoutes.put("/:id", turmaController.update);
-turmaRoutes.delete("/:id", turmaController.delete);
+turmaRoutes.post(
+    "/",
+    requireAuth,
+    authorizeRoles(Papel_usuario.ADM),
+    turmaController.create
+);
+turmaRoutes.get(
+    "/",
+    requireAuth,
+    turmaController.findAll
+);
+turmaRoutes.get(
+    "/:id",
+    requireAuth,
+    turmaController.findById
+);
+turmaRoutes.put(
+    "/:id",
+    requireAuth,
+    authorizeRoles(Papel_usuario.ADM),
+    turmaController.update
+);
+turmaRoutes.delete(
+    "/:id",
+    requireAuth,
+    authorizeRoles(Papel_usuario.ADM),
+    turmaController.delete
+);
 
 export default turmaRoutes;
